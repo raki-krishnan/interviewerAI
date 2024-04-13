2feb0c4f756a4d2c436a54e9bf1ece25372f4d91
raki-krishnan <raki@umich.edu>
2024-04-13 12:15:39 -0400

    setup of directories

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Interview from './Components/InterviewApp/InterviewApp';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Interview />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/InterviewApp/InterviewApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Welcome to My React App!</h1>
+            <p>Start building your awesome application here.</p>
+        </div>
+    );
+}
+
+export default App;
